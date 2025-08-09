d4f44b2862d282629901d7bb66a4c5be4d2798c6
Rondi650 <rondinellenunes123@gmail.com>
2025-08-09 12:28:24 -0300

    Primeirp commit

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+
+restaurantes = ['Strogonoff','Frango com Quiabo']
+
+def exibir_nome_do_programa():
+    print("""
+      
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      """)
+
+def exibirOpcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+    
+def finalizar_app():
+    exibir_subtitulo('Finalizando aplicativo')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu: ')
+    main()
+
+def opcao_invalida():
+    print('Opcao Invalida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'\nO restaurante {nome_do_restaurante} foi cadastrado com sucesso')
+    voltar_ao_menu_principal()
+    
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+    for restaurante in restaurantes:
+         print(f'.{restaurante}')    
+    voltar_ao_menu_principal()
+
+def escolher_opcoes():
+    try:
+        opcao_escolhida = int(input('Escolha uma opcao '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+        elif(opcao_escolhida == 4):
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+    
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibirOpcoes()
+    escolher_opcoes()
+    
+if __name__ == '__main__':
+    main()

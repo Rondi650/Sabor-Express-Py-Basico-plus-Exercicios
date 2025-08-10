7506a6892b105b949b7bdbc4deadaaba4360fdad
Rondi650 <rondinellenunes123@gmail.com>
2025-08-10 18:24:45 -0300

    exercicios resolvidos

--- exc.py
+++ exc.py
@@ -1,3 +1,5 @@
+import os
+
 def exercicio_nome():
       nome = 'Rondi'
       idade = 35
@@ -229,3 +231,137 @@ def media_lista():
     except Exception as e:
         print(f'Ocorreu um erro ao calcular a média: {e}')
 
+
+def dicionario_basico():
+# 1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+# 2 - Utilizando o dicionário criado no item 1:
+      # Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+      # Adicione um campo de profissão para essa pessoa;
+      # Remova um item do dicionário.
+
+            
+      def exibirOpcoes():
+            print('1. Mostrar Tabela')
+            print('2. Alterar Dados lista')
+            print('3. Remover item da lista')
+            print('3. Sair')
+ 
+            
+      def main():
+            exibirOpcoes()
+            
+            opcao_escolhida = int(input('Escolha uma opcao '))
+            
+            if opcao_escolhida == 1:
+                  tabela()
+            if opcao_escolhida == 2:
+                  alterar_dados_lista()     
+            if opcao_escolhida == 3:
+                  remover_item_da_lista()                                    
+            if opcao_escolhida == 4:
+                  sair()                     
+                              
+
+      informacoes_pessoais = [{'nome': 'Rondinelle','idade': 35,'cidade': 'Vitoria'},
+                              {'nome': 'Samara','idade': 28,'cidade': 'Belo Horizonte'},
+                              {'nome': 'Heitor','idade': 7,'cidade': 'Belo Horizonte'}]
+
+      def titulo():
+            titulo = f'{'NOME'.ljust(20)} | {'IDADE'.ljust(20)} | {'CIDADE'.ljust(20)} | {'PROFISSAO'}'
+            print(titulo)
+            print('-' * (len(titulo)+10))
+      
+      def tabela():
+            titulo()
+            try:
+                  for name in informacoes_pessoais:
+                        nome = name.get('nome', '')
+                        idade = str(name.get('idade', ''))
+                        cidade = name.get('cidade', '')
+                        profissao = name.get('profissao', '')
+                        print(f"{nome.ljust(20)} | {idade.ljust(20)} | {cidade.ljust(20)} | {profissao}")
+            except Exception as e:
+                        print(f'Erro: {e}')
+            
+            main()
+      
+      def alterar_dados_lista():
+            nome_validacao = input('Digite o nome da pessoa que deseja atualizar a idade: ')
+            idade = int(input('Atualize a idade: '))
+            profissao = input('Digite o nome da profissao: ')
+            
+            for dado_lista in informacoes_pessoais:
+                  if nome_validacao == dado_lista['nome']:
+                        dado_lista['idade'] = idade
+                        dado_lista['profissao'] = profissao
+                        
+            main()     
+            
+            
+      def remover_item_da_lista():
+            nome_validacao = input('Digite o nome da pessoa que deseja remover algum item: ')    
+            item_removido =   input('Digite o nome do item que deseja remover: ')      
+            
+            for nome in informacoes_pessoais:
+                  if nome_validacao == nome['nome']:
+                        if item_removido in nome:
+                              nome.pop(item_removido) # remove todo o campo dentro do dicionario
+                              nome[item_removido] = None  # Coloca None no valor escolhido
+                              
+            main() 
+
+      def sair ():
+            os.system('cls')
+
+      if __name__ == '__main__':
+            main()
+
+
+def quadrados():
+# 3 - Crie um dicionário que relacione os números de 1 a 5 aos seus respectivos quadrados.
+
+      # Com dictionary comprehension:
+      numeros_quadrados = {x: x**2 for x in range(1, 6)}
+      print(numeros_quadrados)
+
+      # Sem dictionary comprehension:
+      numeros_quadrados = {}
+      for x in range(1, 6):
+            numeros_quadrados[x] = x**2
+      print(numeros_quadrados)
+
+
+def validacao_dicionario():
+# 4 - Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+
+      pessoa = {'nome': 'Amanda', 'idade': 19, 'cidade': 'São Luís'}
+      if 'nome' in pessoa:
+            print("A chave 'nome' existe no dicionário.")
+      else:
+            print("A chave 'nome' não existe no dicionário.")
+
+
+def verificacao_frequencia_palavras():
+# 5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+
+      frase = "Python se tornou uma das linguagens de programação mais populares do mundo nos últimos anos."
+      contagem_palavras = {}
+      palavras = frase.split()
+
+      for palavra in palavras:
+            contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+      
+      print(contagem_palavras)
+
+      # mesma coisa mais simples de entender
+      frase = "Python se tornou uma das linguagens de programação mais populares do mundo nos últimos anos."
+      contagem_palavras = {}
+      palavras = frase.split()
+
+      for palavra in palavras:
+            if palavra in contagem_palavras:
+                  contagem_palavras[palavra] += 1
+      else:
+            contagem_palavras     
+
+      print(contagem_palavras)
